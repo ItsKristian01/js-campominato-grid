67b0236809ead30df64a7770a54b6e1144251834
ItsKristian01 <kristianhana11@gmail.com>
2023-10-27 16:42:56 +0200

    add: element created to HTML

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // Variable Declaration
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
   const divCol = document.createElement("div");
   divCol.classList.add("col");
   divCol.innerHTML = i;
   console.log(divCol);
+  const element = document.getElementById("numbers");
+  element.append(divCol);
 }
+
+
+
