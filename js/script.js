6abaf052b547b13cd9bff8e4f8dd9325bbb157f6
ItsKristian01 <kristianhana11@gmail.com>
2023-10-27 16:47:04 +0200

    doc: add comments

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-// Variable Declaration
+// Numbers generator
 
 for (let i = 1; i <= 100; i++) {
-  const divCol = document.createElement("div");
-  divCol.classList.add("col");
+  const divCol = document.createElement("div");   // Element Creation
+  divCol.classList.add("col"); // Added class
   divCol.innerHTML = i;
   console.log(divCol);
-  const element = document.getElementById("numbers");
-  element.append(divCol);
+  const element = document.getElementById("numbers"); 
+  element.append(divCol); // Added element to HTML
 }
+
+
+
+
 
 
 
